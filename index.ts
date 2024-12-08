29b5f0ac24e8057a44ab4785e72a948c9f8dacfa
Sean Derrow <sean.derrow@gmail.com>
2024-12-08 11:12:10 -0500

    Fully working

--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
+import { DateTime } from "luxon";
 import { aoc23d01p1, aoc23d01p2 } from "./src/23d01";
 
-const main = async () => {
-  const s = process.hrtime();
-  await aoc23d01p1();
-  await aoc23d01p2();
-  const e = process.hrtime(s);
-  console.log(`Time: ${e[0]}s ${(e[1] / 1e6).toFixed(2)}ms`);
-};
-
-main();
+const s = process.hrtime();
+await aoc23d01p1();
+await aoc23d01p2();
+const e = process.hrtime(s);
+console.log(`Time: ${e[0]}s ${(e[1] / 1e6).toFixed(2)}ms`);
+console.log(`Done at ${DateTime.now().toISO()}`);
