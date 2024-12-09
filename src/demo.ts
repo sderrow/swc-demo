f2794b4d495d629049e39657b917589ffce019bc
Sean Derrow <sean.derrow@gmail.com>
2024-12-09 12:19:36 -0500

    Nested callers

--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,6 @@
 import { readFile } from "fs/promises";
 import { DateTime } from "luxon";
+import { nestedCaller } from "./f2/nestedCaller";
 
 export const runDemo = async () => {
   const text = await readFile("./input/test.txt", "utf-8");
@@ -7,4 +8,6 @@ export const runDemo = async () => {
   for (const line of lines) {
     console.log(`${DateTime.now().toISO()}: ${line}`);
   }
+  // Try nested file imports
+  nestedCaller();
 };
